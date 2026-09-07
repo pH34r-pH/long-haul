38a46caa7b299f508d561d4ca3d1a8ff2fd6cff2
Tyler Harbin-Giuntoli <harbin.giuntoli@gmail.com>
2026-09-07 16:49:21 -0700

    Initialize package

---
+++ src/long_haul/__init__.py
@@ -0,0 +1,5 @@
+"""Long Haul coordination primitives."""
+
+from .models import CrewMember, Decision, ExecutionMode, ExecutionPlan, Vessel
+
+__all__ = ["CrewMember", "Decision", "ExecutionMode", "ExecutionPlan", "Vessel"]
