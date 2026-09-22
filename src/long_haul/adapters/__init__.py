74c9bf41b806a593496d4a7306f7ef4bc00e35c0
Tyler Harbin-Giuntoli <harbin.giuntoli@gmail.com>
2026-09-21 23:43:31 -0700

    Revert premature Pi adapter exports (#62)

--- src/long_haul/adapters/__init__.py
+++ src/long_haul/adapters/__init__.py
@@ -1,13 +1,3 @@
 from .llama_cpp import LlamaCppAdapter
-from .pi import FakePiBackend, PiAdapter, PiBackend, PiEvent, PiEventKind, PiRequest, PiRunResult
 
-__all__ = [
-    "FakePiBackend",
-    "LlamaCppAdapter",
-    "PiAdapter",
-    "PiBackend",
-    "PiEvent",
-    "PiEventKind",
-    "PiRequest",
-    "PiRunResult",
-]
+__all__ = ["LlamaCppAdapter"]
