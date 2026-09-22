674484f8588e8a3361c4904a0d8dd0bff4898459
Tyler Harbin-Giuntoli <harbin.giuntoli@gmail.com>
2026-09-21 23:39:07 -0700

    Export Pi adapter primitives (#62)

--- src/long_haul/adapters/__init__.py
+++ src/long_haul/adapters/__init__.py
@@ -1,3 +1,13 @@
 from .llama_cpp import LlamaCppAdapter
+from .pi import FakePiBackend, PiAdapter, PiBackend, PiEvent, PiEventKind, PiRequest, PiRunResult
 
-__all__ = ["LlamaCppAdapter"]
+__all__ = [
+    "FakePiBackend",
+    "LlamaCppAdapter",
+    "PiAdapter",
+    "PiBackend",
+    "PiEvent",
+    "PiEventKind",
+    "PiRequest",
+    "PiRunResult",
+]
